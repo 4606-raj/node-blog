44797cfec8c4a030e084b2743ea025ace7d4f2ec
satya <raj.kumar@anviam.com>
2022-07-06 15:52:35 +0530

    improvement

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -3,7 +3,7 @@ module.exports.successResponse = (res, data, message = 'data found successfully'
     res.status(200).send(response)
 }
 
-module.exports.errorResponse = (res, message = 'data found successfully', code = 404, status = false) => {
-    let response = { message, status }
+module.exports.errorResponse = (res, message = 'data found successfully', code = 404, errors = [], status = false) => {
+    let response = { message, errors, status }
     res.status(code).send(response)
 }
--- middleware/request-validator.js
+++ middleware/request-validator.js
@@ -1,10 +1,15 @@
-const {validationResult} = require('express-validator')
+const {validationResult} = require('express-validator');
+const { errorResponse } = require('../helpers/helpers');
 
 const validateRequest = (req, res, next) => {
     let errors = validationResult(req);
     
     if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        let response = {
+            errors: errors.array()
+        }
+        return errorResponse(res, 'validation error', 400, errors.array());
+        // return res.status(400).json({ errors: errors.array() });
     }
 
     next();
